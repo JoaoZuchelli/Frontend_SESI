61089a99f8b34dcea2aab740c043f8e5b9fd7e0b
JoaoZuchelli <zuchellijoaogabriel@gmail.com>
2026-05-22 12:12:21 -0300

    continuaçao

--- script.js
+++ script.js
@@ -1,40 +1,71 @@
-function executarSistema(){
-    const nome = document.getElementById("inputNome").value;
-    const idade = parseInt(document.getElementById("inputIdade").value);
-    const valor = parseFloat(document.getElementById("inputValor").value);
-    const cupom = document.getElementById("inputCupom").value === "true";
+function executarSistema() {
+    // tratamento de erros para o sistema nao parar de funcionar
 
+    try {
+        //dados
+        const inputNome = document.getElementById("inputNome");
+        const inputIdade = (document.getElementById("inputIdade"));
+        const inputValor = (document.getElementById("inputValor"));
+        const inputCupom = document.getElementById("inputCupom");
 
+        // Dados de saída
+        const msg = document.getElementById("mensagem-autorizacao");
+        const lista = document.getElementById("lista-estoque");
+        const relatorio = document.getElementById("relatorio-final");
 
-// DADOS DE SAIDA
+        const btn = document.getElementById("btnfinalizar")
 
-    const msg = document.getElementById("mensagem-autorizacao");
-    const lista = document.getElementById("lista-estoque");
-    const relatorio = document.getElementById("relatorio-final");
+        btn.disable = true;
+        btn.innerText = "Processando..."
 
-// validaçao para cupom vazios
+            //remove os espaçoes em branco
+        const nome = inputNome.value.trim();
+        const idade = parseInt.inputIdade.value;
+        const valor = parseFloat.inputValor.value;
+        const cupom = inputCupom.value === "true";
+        
 
-if (!nome || isNaN(idade) || isNaN(valor)){
-    alert("Por favor. preencha todos os campos: ");
-    return;
+        // Validação para campos vazios
+        if (!nome || isNaN(idade) || isNaN(valor)) {
+           msg.innerText="Preencha todos os campos corretamente";
+           msg.style.color = ""
+            return;
+        }
+
+        // Regra de negócio
+        if (idade >= 16) {
+            msg.innerText = `Venda autorizada: ${nome}`;
+            msg.style.color = " ";
+
+            // Desconto
+            let valorFinal = valor > 500 || cupom ? valor * 0.85 : valor;
+
+            // Estoque
+            let estoque = ["Placa de Vídeo", "Processador", "Memória RAM"];
+            lista.innerHTML = ""; // Limpa a lista anterior
+
+            // forEach: Percorre um array e aplica uma ação para cada elemento
+            estoque.forEach((item) => {
+                let li = document.createElement("li");
+                li.innerText = `Item ${item} reservado.`;
+                lista.appendChild(li); // usado para adicionar um novo elemento ou texto
+            });
+
+            // Relatório
+            relatorio.style.display = "block";
+            relatorio.innerHTML = `
+                <strong> RESUMO DO PEDIDO <\strong><br>
+                Cliente: ${nome} <br>
+                Total Original: R$ ${valor.toFixed(2)} <br>
+                <strong> Total com Desconto: R$ ${valorFinal.toFixed(
+                2
+            )} <\strong>
+            `;
+        } else {
+            msg.innerText = "Venda bloqueada: Menor de 16 anos.";
+            msg.style.color = "#ff4444";
+            relatorio.style.display = "none";
+            lista.innerHTML = "";
+        }
+    } catch (error) { }
 }
- 
-}
-
-// Regras de negocio
-
-if(idade >= 16) {
-    msg.innerText = `Venda autorizada: ${nome}`;
-    msg.style.color = "#b40d0d";
-
-
-    // Desconto
-
-    let valorifinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
-
-    // Estoque
-
-    let estoque = ("Placa de Video", "Processador", "Memoria RAM");
-    lista.innerHTML = ""; // Limpa a Lista aneterior
-} 
-
