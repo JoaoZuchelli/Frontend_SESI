7e098c4e0efea8d2f53d2d09712b40e144bbd67f
JoaoZuchelli <zuchellijoaogabriel@gmail.com>
2026-06-11 12:10:49 -0300

    nao sei mais de nada

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function executarSistema() {
-    // tratamento de erros para o sistema nao parar de funcionar
-
+ 
     try {
-        //dados
+      
         const inputNome = document.getElementById("inputNome");
         const inputIdade = (document.getElementById("inputIdade"));
         const inputValor = (document.getElementById("inputValor"));
         const inputCupom = document.getElementById("inputCupom");
 
-        // Dados de saída
+    
         const msg = document.getElementById("mensagem-autorizacao");
         const lista = document.getElementById("lista-estoque");
         const relatorio = document.getElementById("relatorio-final");
@@ -18,40 +17,35 @@ function executarSistema() {
         btn.disable = true;
         btn.innerText = "Processando..."
 
-            //remove os espaçoes em branco
         const nome = inputNome.value.trim();
         const idade = parseInt.inputIdade.value;
         const valor = parseFloat.inputValor.value;
         const cupom = inputCupom.value === "true";
         
-
-        // Validação para campos vazios
         if (!nome || isNaN(idade) || isNaN(valor)) {
            msg.innerText="Preencha todos os campos corretamente";
            msg.style.color = ""
             return;
         }
 
-        // Regra de negócio
         if (idade >= 16) {
             msg.innerText = `Venda autorizada: ${nome}`;
             msg.style.color = " ";
 
-            // Desconto
             let valorFinal = valor > 500 || cupom ? valor * 0.85 : valor;
 
-            // Estoque
+     
             let estoque = ["Placa de Vídeo", "Processador", "Memória RAM"];
-            lista.innerHTML = ""; // Limpa a lista anterior
+            lista.innerHTML = ""; 
 
-            // forEach: Percorre um array e aplica uma ação para cada elemento
+
             estoque.forEach((item) => {
                 let li = document.createElement("li");
                 li.innerText = `Item ${item} reservado.`;
-                lista.appendChild(li); // usado para adicionar um novo elemento ou texto
+                lista.appendChild(li);
             });
 
-            // Relatório
+     
             relatorio.style.display = "block";
             relatorio.innerHTML = `
                 <strong> RESUMO DO PEDIDO <\strong><br>
